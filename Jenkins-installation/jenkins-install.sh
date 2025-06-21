06552866f26118d701ba6d0ed57f2ff74e0aa814
Titalearn <126238347+Titalearn@users.noreply.github.com>
2025-06-21 15:55:36 +0200

    Update jenkins-install.sh

--- Jenkins-installation/jenkins-install.sh
+++ Jenkins-installation/jenkins-install.sh
@@ -1,4 +1,4 @@
-sudo yum install wget git nano 
+sudo yum install wget git nano unzip -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
